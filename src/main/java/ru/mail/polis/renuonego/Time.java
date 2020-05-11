f34d86b21c2b2784030ed0d5350be06c564eff7e
dmitry.nazarov <dmitry.nazarov@corp.mail.ru>
2020-05-11 12:10:01 +0300

    add utility class with currentTimeInNano() method

---
+++ src/main/java/ru/mail/polis/renuonego/Time.java
@@ -0,0 +1,25 @@
+package ru.mail.polis.renuonego;
+
+final class Time {
+    private static int countNanos;
+    private static long lastTimeInMillis;
+
+    private Time() {
+    }
+
+    /**
+     * Checks current time in millis and counts nanoseconds.
+     *
+     * @return Returns current time in nano seconds
+     */
+    static long currentTimeInNano() {
+        synchronized (Time.class) {
+            final long currentTimeMillis = System.currentTimeMillis();
+            if (lastTimeInMillis != currentTimeMillis) {
+                lastTimeInMillis = currentTimeMillis;
+                countNanos = 0;
+            }
+            return lastTimeInMillis * 1_000_000 + countNanos++;
+        }
+    }
+}
