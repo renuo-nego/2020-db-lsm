6e8109b3b4e0a38a1d5510b8abb452d0bc6da58a
dmitry.nazarov <dmitry.nazarov@corp.mail.ru>
2020-05-11 12:08:54 +0300

    create Table Interface

---
+++ src/main/java/ru/mail/polis/renuonego/Table.java
@@ -0,0 +1,19 @@
+package ru.mail.polis.renuonego;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+
+interface Table {
+    @NotNull
+    Iterator<Cell> iterator(@NotNull final ByteBuffer from) throws IOException;
+
+    void upsert(@NotNull final ByteBuffer key,
+                @NotNull final ByteBuffer value) throws IOException;
+
+    void remove(@NotNull final ByteBuffer key) throws IOException;
+
+    long sizeInBytes() throws IOException;
+}
